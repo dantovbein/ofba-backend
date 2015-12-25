62b3d833368d29557e049e025c7cf412510f4bd3
Dan Tovbein <dtovbein@gmail.com>
2015-12-25 19:12:27 -0300

    Consulta del evento casi finalizada

--- service/Storage.php
+++ service/Storage.php
@@ -351,12 +351,13 @@ class Storage {
 	public function postEvento($data) {
 		$this->connect();
 		
+		$uidEvento = md5(uniqid(rand(), true));
 		for($i=0;$i<count($data['fechas']);$i++){
 			$uid = md5(uniqid(rand(), true));
 			
 			// insert details data
-			$queryEventDetails = "INSERT INTO ofba_jevents_vevdetail (dtstart,description,summary) VALUES ('".$data['fechas'][$i]."','". $data['desc']."','"."summary"."');";
-			$result = mysql_query($queryEventDetails) or die('Error en la consulta -> ' .  $queryEventDetails);
+			$queryEventDetails = "INSERT INTO ofba_jevents_vevdetail (dtstart,description,summary) VALUES ('".$data['fechas'][$i]."','". $data['desc']."','".""."');";
+			mysql_query($queryEventDetails) or die('Error en la consulta -> ' .  $queryEventDetails);
 			$eventDetailId = mysql_insert_id();
 			
 			// insert data to event
@@ -366,13 +367,13 @@ class Storage {
 			$rawdata = 'a:20:{s:3:"UID";s:32:"'.$uid.'";s:11:"X-EXTRAINFO";s:0:"";s:8:"LOCATION";s:0:"";s:11:"allDayEvent";s:3:"off";s:7:"CONTACT";s:0:"";s:11:"DESCRIPTION";s:'.strlen($data['desc']).':'.$data['desc'].';s:12:"publish_down";s:10:"'.$dt->format('Y-m-d').'";s:10:"publish_up";s:10:"'.$dt->format('Y-m-d').'";s:13:"publish_down2";s:10:"'.$dt->format('Y-m-d').'";s:11:"publish_up2";s:10:"'.$dt->format('Y-m-d').'";s:7:"SUMMARY";s:17:"Giras Extranjeras";s:3:"URL";s:0:"";s:11:"X-CREATEDBY";i:40;s:7:"DTSTART";i:'.$data['fechas'][$i].';s:5:"DTEND";i:'.$data['fechas'][$i].';s:5:"RRULE";a:4:{s:4:"FREQ";s:4:"none";s:5:"COUNT";i:1;s:8:"INTERVAL";s:1:"1";s:5:"BYDAY";s:24:"+1SA,+2SA,+3SA,+4SA,+5SA";}s:8:"MULTIDAY";s:1:"1";s:9:"NOENDTIME";s:1:"1";s:7:"X-COLOR";s:0:"";s:9:"LOCKEVENT";s:1:"0";}';
 			
 			$queryEventData = "INSERT INTO ofba_jevents_vevent (icsid,catid,uid,created_by,modified_by,rawdata,detail_id) VALUES ('"."0"."','"."0"."','". $uid."','"."40"."','"."40"."','".$rawdata."','".$eventDetailId."');";
-			$result = mysql_query($queryEventData) or die('Error en la consulta -> ' .  $queryEventData);
+			mysql_query($queryEventData) or die('Error en la consulta -> ' .  $queryEventData);
 			$eventId = mysql_insert_id();
 			
 			// repetition to get the url
 			$uidRepetition = md5(uniqid(rand(), true));
-			$queryEventRepetition = "INSERT INTO ofba_jevents_repetition (eventid,eventdetail_id,duplicatecheck,startrepeat,endrepeat) VALUES ('".$eventId."','".$eventDetailId."','". $uidRepetition."','".$dt->format('Y-m-d H:m:s')."','".$dt->format('Y-m-d H:m:s')."');";
-			$result = mysql_query($queryEventRepetition) or die('Error en la consulta -> ' .  $queryEventRepetition);
+			$queryEventRepetition = "INSERT INTO ofba_jevents_repetition (eventid,eventdetail_id,duplicatecheck,startrepeat,endrepeat) VALUES ('".$eventId."','".$eventDetailId."','".$uidRepetition."','".$dt->format('Y-m-d H:m:s')."','".$dt->format('Y-m-d H:m:s')."');";
+			mysql_query($queryEventRepetition) or die('Error en la consulta -> ' .  $queryEventRepetition);
 			$eventRepetitionId = mysql_insert_id();
 			$link = "index.php/homepage/listados-completo/icalrepeat.detail/".$dt->format('Y')."/".$dt->format('m')."/".$dt->format('d')."/".$eventRepetitionId;
 
@@ -381,12 +382,43 @@ class Storage {
 			$result = mysql_fetch_assoc(mysql_query($queryLastEvento));
 			$lastEventPerfxId = $result['id'];
 			$newEeventoPerfxId = $lastEventPerfxId+1;
-			$queryEventoPerfx = "INSERT INTO eventos_perfx (id,eventid,idTemporada,idCiclo,fecha,horaInicio,link,idLocacion,idpais,giras_nacionalidad) VALUES ('".$newEeventoPerfxId."','".$eventId."','".$data['temporada']."','".$data['ciclo']."','".$dt->format('Y-m-d')."','".$dt->format('H:m:s')."','".$link."','".$data['locacion']."','".$data['pais']."','".$data['nacionalidad']."');";
-			$result = mysql_query($queryEventoPerfx) or die('Error en la consulta -> ' .  $queryEventoPerfx);
+			$queryEventoPerfx = "INSERT INTO eventos_perfx (id,eventid,uidEvento,idTemporada,idCiclo,fecha,horaInicio,link,idLocacion,idpais,giras_nacionalidad) VALUES ('".$newEeventoPerfxId."','".$eventId."','".$uidEvento."','".$data['temporada']."','".$data['ciclo']."','".$dt->format('Y-m-d')."','".$dt->format('H:m:s')."','".$link."','".$data['locacion']."','".$data['pais']."','".$data['nacionalidad']."');";
+			mysql_query($queryEventoPerfx) or die('Error en la consulta -> ' .  $queryEventoPerfx);
+		}
+
+		// insert imagen of the event
+		$queryEventImage = "INSERT INTO imagenes_evento (uidEvento,imagen) VALUES ('".$uidEvento."','". $data['imagen']."');";
+		mysql_query($queryEventImage) or die('Error en la consulta -> ' .  $queryEventImage);
+
+		// insert directores
+		for($d=0;$d<count($data['directores']);$d++) {
+			$queryEventDirectores = "INSERT INTO directores_evento (idEvento,uidEvento,idDirector) VALUES ('0','".$uidEvento."','". $data['directores'][$d]."');";
+			mysql_query($queryEventDirectores) or die('Error en la consulta -> ' .  $queryEventDirectores);
+		}
+
+		// insert solistas
+		for($s=0;$s<count($data['solistas']);$s++) {
+			$queryEventSolistas = "INSERT INTO solistas_evento (idEvento,uidEvento,idSolista) VALUES ('0','".$uidEvento."','". $data['solistas'][$s]."');";
+			mysql_query($queryEventSolistas) or die('Error en la consulta -> ' .  $queryEventSolistas);
+		}
+
+		// insert compositores
+		for($c=0;$c<count($data['compositores']);$c++) {
+			$queryEventCompositores = "INSERT INTO compositores_evento (idEvento,uidEvento,idCompositor) VALUES ('0','".$uidEvento."','". $data['compositores'][$c]->id."');";
+			$result = mysql_query($queryEventCompositores) or die('Error en la consulta -> ' .  $queryEventCompositores);
+			// Insert obras from each one
+			for($o=0;$o<count($data['compositores'][$c]->obras);$o++) {
+				$queryObraCompositor = "INSERT INTO obras_evento (idObra,uidEvento,idCompositor) VALUES ('".$data['compositores'][$c]->obras[$o]."','".$uidEvento."','".$data['compositores'][$c]->id."')";
+				mysql_query($queryObraCompositor) or die('Error en la consulta -> ' .  $queryObraCompositor);
+			}
+		}
+
+		// insert solistas
+		for($t=0;$t<count($data['textos']);$t++) {
+			$queryTextosEvento = "INSERT INTO textos_evento (uidEvento,texto,orden) VALUES ('".$uidEvento."','". $data['textos'][$t]."','". ($t+1)."');";
+			mysql_query($queryTextosEvento) or die('Error en la consulta -> ' .  $queryTextosEvento);
+		}
 			
-			echo "detail: " . $eventDetailId . " - event: " . $eventId . " - repetition: " . $eventRepetitionId . " - eventos_perfx: " . $newEeventoPerfxId . " - link: ". $link .'</br>';
-			//echo "</br>";
-		}		
 		$this->close();
 	}
 
--- service/manager/postEvento.php
+++ service/manager/postEvento.php
@@ -3,8 +3,6 @@
 	
 	require_once "../Storage.php";	
 	
-	$uid = md5(uniqid(rand(), true));
-
 	$titulo = (isset($_GET['titulo'])) ? $_GET['titulo'] : "";
 	$imagen = (isset($_GET['imagen'])) ? $_GET['imagen'] : "";
 	$ciclo = (isset($_GET['ciclo'])) ? $_GET['ciclo'] : "";
@@ -13,14 +11,61 @@
 	$pais = (isset($_GET['pais'])) ? $_GET['pais'] : "";
 	$nacionalidad = (isset($_GET['nacionalidad'])) ? $_GET['nacionalidad'] : "";
 	$temporada = (isset($_GET['temporada'])) ? $_GET['temporada'] : "";
-	$fechas = (isset($_GET['fechas'])) ? $_GET['fechas'] : "";
+	$fechas = (isset($_GET['fechas'])) ? json_decode($_GET['fechas']) : array();
 	$director = (isset($_GET['director'])) ? $_GET['director'] : "";
-	$extra = (isset($_GET['extra'])) ? json_decode($_GET['extra']) : [];
-	if(count($extra)==0) {
-		//$extra->textos = [];
-	}
+
+	$extra = json_decode($_GET['extra']);
+	$textos = $extra->textos;
+	$directores = $extra->directores;
+	$compositores = $extra->compositores;
+	$solistas = $extra->solistas;
+
 	$desc = (isset($_GET['desc'])) ? $_GET['desc'] : "";
+
+	//$textos = (isset($_GET['textos'])) ? json_decode($_GET['textos']) : array();
+	//$compositores = (isset($_GET['compositores'])) ? json_decode($_GET['compositores']) : array();
 	
+	//for($i=0;$i<count($compositores);$i++){
+		//echo $compositores[$i];
+	//	var_dump($compositores);
+	//}
+
+	//var_dump($extra);
+
+	//for($i=0;$i<count($textos);$i++){
+	//	echo $textos[$i] . '</br>';
+	//}
+	//var_dump($compositores);
+
+	//for($i=0;$i<count($compositores);$i++){
+		//var_dump($compositores[$i]);
+		//var_dump($compositores[$i]->obras);
+		//echo count($compositores[$i]->obras) . '</br>';
+	//}
+
+	//echo count($compositores[3]->obras);
+	//echo gettype($textos);
+	//echo isset($_GET['textos']);
+	//echo gettype($textos);
+	//echo $_GET['textos'];
+	//$textos = json_decode($_GET['fechas']);
+	
+	//$extra = $_GET['extra'];
+	//$extra = stripcslashes($extra);
+	//$extra = json_decode($extra);
+	//echo gettype($extra);
+	//echo $textos;
+	
+
+	//$textos = json_decode($extra->textos);
+	//echo mysql_fetch_object($extra);
+	//if(count($extra)==0) {
+		//$extra->textos = [];
+	//}
+	
+	
+	//echo $extra;
+
 	/* start test */
 	//$test = '{"titulo":"Titulo de mi evento","imagen":"","ciclo":"1","locacion":"2","ciudad":"2","pais":"0","nacionalidad":"1","temporada":"4","fechas":["1449799860","1449163320"],"director":"70","extra":{"textos":["Este es el texto 1","Este es el texto 2","Este es el texto 3","Este es el texto 4 adicional"],"directores":["61","60","5"],"compositores":[{"id":"120","obras":["62"]},{"id":"113","obras":["3","2","1"]},{"id":"95","obras":["36"]}],"solistas":["55","29","85"]},"desc":"<p>{source}[[div class=\"row-fluid\"]][[div class=\"span12\"]][[img class=\"pull-right img-ofba\" src=\"images/ofba/events/thumbs/\" alt=\"\" /]][[h4]]Temporada 2011[[/h4]][[h5]]Ciclo abono[[/h5]][[h5]]Locacion usina arte[[/h5]][[h5]]Rosario, Argentina[[/h5]][[/br]][[p]][[strong]]Director: [[a href=\"\"]]Julio Fainguersch[[/a]][[/strong]][[/p]][[/br]][[p]][[h4]]Directores:[[/h4]][[span]]César Bustamante[[/span]], [[span]]Miguel Ángel Pesce[[/span]], [[span]]Maximiliano Valdés[[/span]][[/p]][[/br]][[p]][[h4]]Solistas:[[/h4]][[span]]Conjunto Instrumental 2[[/span]], [[span]]Karin Lechner[[/span]], [[span]]Algirdas Janutas[[/span]][[/p]][[/br]][[p]][[h4]]Compositores:[[/h4]][[strong]]Carlos Chavez[[/strong]]: [[ul]][[li]]Concierto para violín[[/li]][[/ul]][[strong]]Wolfgang Amadeus Mozart[[/strong]]: [[ul]][[li]]Concierto Nº 20 para piano y orquesta en Re menor, K. 466[[/li]][[li]]Obertura de La flauta mágica, K. 620[[/li]][[li]]Concierto Nº 14 para piano y orquesta en Mi bemol Mayor, K. 449[[/li]][[/ul]][[strong]]George Bizet[[/strong]]: [[ul]][[li]]Les dragons dAlcala y La garde montante, de las Suites No 1 y 2 de Carmen[[/li]][[/ul]][[/p]][[/br]][[p]]Este es el texto 1[[/p]][[/br]][[p]]Este es el texto 2[[/p]][[/br]][[p]]Este es el texto 3[[/p]][[/br]][[p]]Este es el texto 4 adicional[[/p]][[/br]][[/div]][[/div]]{/source}</p>"}';
 	//$titulo = "Titulo de mi evento";
@@ -49,15 +94,12 @@
     	"temporada" => $temporada,
     	"fechas" => $fechas,
     	"director" => $director,
-    	"extra" => $extra,
+    	"textos" => $textos,
+    	"directores" => $directores,
+    	"compositores" => $compositores,
+    	"solistas" => $solistas,
     	"desc" => $desc
     );
-
-	//echo print_r($extra);
-	//echo $desc;
-	//echo print_r($extra->textos[1]);
-	//echo print_r(count($extra->directores));
-	//echo $desc;
 
 	$storage = new Storage();
 	echo $storage->postEvento($data);
