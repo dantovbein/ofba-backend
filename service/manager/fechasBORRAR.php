602e90a952371a714e4b5b8cbaeb37a0be38e384
Dan Tovbein <dtovbein@gmail.com>
2015-12-28 08:44:57 -0300

    pruebas @epoch

---
+++ service/manager/fechasBORRAR.php
@@ -0,0 +1,15 @@
+<?php
+
+//Mon Dec 28 2015 08:32:32 GMT-0300 (ART) 1451302352714
+// ddbb 1394062200
+
+$fecha = new DateTime();
+$fecha->setTimestamp(1451302352);
+
+
+//echo $fecha->format('U = Y-m-d H:i:s');
+
+$date = new DateTime();
+//echo $date->getTimestamp();
+echo $date->getdate();
+?>
